5b54dc441ff24f53a91222a8745a6bb03f7bbd48
Wei Ji <weiji@Weis-MacBook-Pro.local>
2024-02-05 22:38:52 -0800

    cleaned tests

--- tests/obsolete/testIMessage.js
+++
@@ -1,9 +0,0 @@
-import { runAppleScript } from 'run-applescript';
-
-(async () => {
-    const result = await runAppleScript(`do shell script "sqlite3 ~/Library/Messages/chat.db 'select text from message order by date desc limit 1;'"`);
-    const match = result.match(/\d{6}/);
-    console.log(match[0]);
-    return match ? match[0] : null;
-    
-})();
